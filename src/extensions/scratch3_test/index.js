0e75d22400d7de1a66b5ff2bf727d04d6383ea43
mshinke <mshinke@users.sourceforge.jp>
2018-12-04 21:27:01 +0900

    Create index.js

---
+++ src/extensions/scratch3_test/index.js
@@ -0,0 +1,21 @@
+//Scratch3TestBlocks
+class Scratch3TestBlocks {
+  constructor() {}
+  getInfo() {
+    return {
+      id: 'test',
+      name: 'Test',
+      blocks: [
+        {
+          opcode: 'ts',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'hello'
+        }
+      ]
+    }
+  }
+  ts() {
+    console.log('ts');
+  }
+}
+module.exports = Scratch3TestBlocks;

03b6b5db4c7b9db72ea53593a9199dbbe88438b2
mshinke <mshinke@users.sourceforge.jp>
2018-12-05 07:39:04 +0900

    Update index.js

--- src/extensions/scratch3_test/index.js
+++ src/extensions/scratch3_test/index.js
@@ -1,20 +1,47 @@
+//const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+//const Cast = require('../../util/cast');
+//const Clone = require('../../util/clone');
+//const Color = require('../../util/color');
+const formatMessage = require('format-message');
+//const MathUtil = require('../../util/math-util');
+//const RenderedTarget = require('../../sprites/rendered-target');
+//const log = require('../../util/log');
+//const StageLayering = require('../../engine/stage-layering');
+
 class Scratch3TestBlocks {
-  constructor() {}
-  getInfo() {
-    return {
-      id: 'test',
-      name: 'Test',
-      blocks: [
-        {
-          opcode: 'ts',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'hello'
-        }
-      ]
+    constructor (runtime) {
+        this.runtime = runtime;
     }
-  }
-  ts() {
-    console.log('ts');
-  }
+
+    getInfo () {
+        return {
+            id: 'test',
+            name: formatMessage({
+                id: 'test.categoryName',
+                default: 'Test',
+                description: 'Test extension'
+            }),
+            blocks: [
+                {
+                    opcode: 'test',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'test.test',
+                        default: 'Test',
+                        description: 'Test block'
+                    })
+                }
+            ],
+            menus: {
+
+	    }
+        };
+    }
+
+    test () {
+      //
+    }
 }
+
 module.exports = Scratch3TestBlocks;
